aa24ac30260212dd91ca0f464de9b3f67adfeb03
darthchudi <chukwudioranu@ymail.com>
2018-12-23 06:51:10 +0100

    chore: remove comment

--- config.js
+++ config.js
@@ -3,5 +3,3 @@ module.exports = {
   PORT: process.env.PORT || 3005,
   MONGODB_URL: process.env.MONGODB_URL || 'mongodb://localhost:27017/cafeone',
 };
-
-//"mongodb://cafe_one:fPLsyXXxxGKs65m@ds013589.mlab.com:13589/cafe_one"
