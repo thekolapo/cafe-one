df9a14d6720c983ec662457aef2b1787cb4bef36
darthchudi <chukwudioranu@ymail.com>
2018-12-23 06:46:52 +0100

    fix: render html file for all paths

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ class App {
 
     this.app.use(express.static(path.join(__dirname, 'frontend')));
 
-    this.app.get('/', (req, res) => {
+    this.app.get('*', (req, res) => {
       res.sendFile(path.join(__dirname, 'frontend/index.html'));
     });
 
