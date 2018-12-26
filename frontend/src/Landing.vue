4f25a4160bc7bcc174ed690d7bdbaf52fe3c0a74
darthchudi <chukwudioranu@ymail.com>
2018-12-26 14:04:59 +0100

    feat: make api requests to backend for registration

--- frontend/src/Landing.vue
+++ frontend/src/Landing.vue
@@ -1,39 +1,138 @@
 <template>
-  <div id = "landing">
-    <div id="white-overlay" ref="whiteOverlay"></div>
+  <div id="landing">
+    <div
+      id="white-overlay"
+      ref="whiteOverlay"
+    ></div>
     <div id="nav">
       <div class="social-links">
-        <a href="" target="_blank">Fb</a><br>
-        <a href="https://twitter.com/cafeone_ng/" target="_blank">Tw</a><br>
-        <a href="https://www.instagram.com/cafeone_nig/" target="_blank">Ig</a>
+        <a
+          href=""
+          target="_blank"
+        >Fb</a><br>
+        <a
+          href="https://twitter.com/cafeone_ng/"
+          target="_blank"
+        >Tw</a><br>
+        <a
+          href="https://www.instagram.com/cafeone_nig/"
+          target="_blank"
+        >Ig</a>
       </div>
     </div>
-    <div class="image-slide-section" ref="bigImageHolder">
-      <img :src="currentImg" ref="bigImage" class="big-image">
+    <div
+      class="image-slide-section"
+      ref="bigImageHolder"
+    >
+      <img
+        :src="currentImg"
+        ref="bigImage"
+        class="big-image"
+      >
       <div class="logo-slogan">
-        <img src="./assets/img/logo.png" class="cafe-one-logo">
+        <img
+          src="./assets/img/logo.png"
+          class="cafe-one-logo"
+        >
         <div class="slogan">Innovation meets community </div>
-      </div>    
+      </div>
     </div>
     <div class="right-side">
+      <toast
+        :message="toast.message"
+        :context="toast.context"
+        v-if="toast.show"
+      />
       <div class="content">
-        <form class="form-section" v-if="showForm">
+        <form
+          class="form-section"
+          v-if="showForm"
+        >
           <div class="header-text">Join our mailing list</div>
           <div class="line-divider margin-bottom-56"></div>
-          <div class="label">Your full name</div>
-          <input type="text" class="input-field" v-model="fullName">
-          <div class="label">Email address</div>
-          <input type="text" class="input-field" v-model="email">
-          <div class="label">Phone number</div>
-          <input type="text" class="input-field" v-model="phoneNumber">
+          <div class="input-container">
+            <div class="label">Your full name</div>
+            <input
+              type="text"
+              class="input-field"
+              :class="{error: $v.full_name.$error}"
+              v-model="$v.full_name.$model"
+            >
+            <transition name="slide-fade">
+              <p
+                class="error"
+                v-if="$v.full_name.$error"
+              >Please enter your first name</p>
+            </transition>
+          </div>
+
+          <div class="input-container">
+            <div class="label">Email address</div>
+            <input
+              type="text"
+              class="input-field"
+              :class="{error: $v.email_address.$error}"
+              v-model="$v.email_address.$model"
+            >
+            <transition name="slide-fade">
+              <p
+                class="error"
+                v-if="$v.email_address.$error"
+              >Please enter a valid email address.</p>
+            </transition>
+          </div>
+
+          <div class="input-container">
+            <div class="label">Phone number</div>
+            <input
+              type="text"
+              class="input-field"
+              :class="{error: $v.phone_number.$error}"
+              v-model="$v.phone_number.$model"
+            >
+            <transition name="slide-fade">
+              <p
+                class="error"
+                v-if="$v.phone_number.$error"
+              >Please enter a valid phone number.</p>
+            </transition>
+          </div>
           <div class="checkbox-section">
-            <input type="checkbox" class="checkbox">
+            <input
+              type="checkbox"
+              class="checkbox"
+              v-model="recieve_emails"
+            >
             <span>By ticking this box, you consent to receiving newsletters from Café One.</span>
           </div>
           <!-- <div class="row col-xs-12"></div> -->
-          <button @click="onSubmit()">Join in</button>
+          <transition
+            name="form-fade"
+            mode="out-in"
+          >
+            <input
+              type="submit"
+              @click.prevent="onSubmit()"
+              value="Join In"
+              :disabled="$v.$invalid"
+              v-if="!loading"
+            />
+            <div
+              v-else
+              class="spinner-container"
+            >
+              <looping-rhombuses-spinner
+                :animation-duration="1000"
+                :size="1000"
+                :color="'#000'"
+              />
+            </div>
+          </transition>
         </form>
-        <div class="about" v-else>
+        <div
+          class="about"
+          v-else
+        >
           <div class="header-text">What we do</div>
           <div class="line-divider"></div>
           <div class="long-text">
@@ -51,140 +150,135 @@
           <div class="small-header-text">Enquiries</div>
           <div class="text-contact-details">cafe.one@sterlingbankng.com</div>
           <div class="text-contact-details">09053872954, 09053872953</div>
-          <div class="text-join-community">Join our community and get 10% off coffee on your first visit  <img src="./assets/img/arrow.png" class="logo" @click="displayForm()"> </div>
+          <div class="text-join-community">Join our community and get 10% off coffee on your first visit <img
+              src="./assets/img/arrow.png"
+              class="logo"
+              @click="displayForm()"
+            > </div>
         </div>
       </div>
     </div>
-    <!-- <div class="row col-xs-12"></div>
-    <form class="section-subscribe" v-if="showForm">
-      <div class="form-mobile">
-        <input type="text" class="text-field" placeholder="Full name" v-model="fullName" required>
-        <input type="email" class="text-field" placeholder="Email address" v-model="email" required>
-        <input type="number" class="text-field" placeholder="Phone number" v-model="phoneNumber" required>
-        <button @click="onSubmit()">Submit</button>
-      </div>
-      <div class="form-non-mobile">
-        <input type="text" class="text-field" placeholder="Full name" v-model="fullName" required>
-        <div class="divider"></div>
-        <input type="email" class="text-field" placeholder="Email address" v-model="email" required>
-        <div class="divider"></div>
-        <input type="number" class="text-field" placeholder="Phone number" v-model="phoneNumber" required>
-        <button @click="onSubmit()">Submit</button>
-        <div class="checkbox-section">
-          <input type="checkbox">
-          <span>Send me occassional marketing updates.</span>
-        </div>
-      </div>
-    </form>  -->
-    <!-- <div class="section-join-community" v-else>
-      <div class="button-join" @click="displayForm()">
-        <div class="text">Join</div>
-        <img src="./assets/img/arrow.png">
-      </div>
-      <br>
-      <div class="small-text">Join our community and get 10% off coffee on your first visit</div>
-    </div> -->
   </div>
 </template>
 <script>
-import {TweenMax, Power2, TimelineLite} from "gsap/TweenMax"
-import axios from 'axios'
+import axios from "axios";
+import { required, email, minLength } from "vuelidate/lib/validators";
+import { isMobilePhone } from "validator";
+import { TweenMax, Power2, TimelineLite } from "gsap/TweenMax";
+import { LoopingRhombusesSpinner } from "epic-spinners";
+import upperFirst from "lodash/upperFirst";
+
+const isNigerianMobile = value => isMobilePhone(value, "en-NG");
+
 var imageIndex = 0;
 var imageSwitchInterval = 3300;
-var images = ['https://res.cloudinary.com/daitiebum/image/upload/v1545113548/image-slide-two.jpg', 'https://res.cloudinary.com/daitiebum/image/upload/v1545113548/image-slide-three.jpg', 'https://res.cloudinary.com/daitiebum/image/upload/v1545115024/image-slide-one.jpg'];
+var images = [
+  "https://res.cloudinary.com/daitiebum/image/upload/v1545113548/image-slide-two.jpg",
+  "https://res.cloudinary.com/daitiebum/image/upload/v1545113548/image-slide-three.jpg",
+  "https://res.cloudinary.com/daitiebum/image/upload/v1545115024/image-slide-one.jpg"
+];
 
 export default {
-  data () {
+  data() {
     return {
+      full_name: "",
+      email_address: "",
+      phone_number: "",
+      recieve_emails: false,
       showForm: false,
-      currentImg: 'https://res.cloudinary.com/daitiebum/image/upload/v1545113548/image-slide-one.jpg',
+      loading: false,
+      toast: {
+        show: false,
+        message: "",
+        context: ""
+      },
+      currentImg:
+        "https://res.cloudinary.com/daitiebum/image/upload/v1545113548/image-slide-one.jpg",
       currentNumber: 0,
-      fullName: '',
-      email:'',
-      phoneNumber:'',
       timer: null
-    }
+    };
   },
 
-  mounted() { 
-    const { bigImage, whiteOverlay } = this.$refs;
+  validations: {
+    full_name: { required },
+    email_address: { required, email },
+    phone_number: { required, isNigerianMobile }
+  },
 
+  mounted() {
+    const { bigImage, whiteOverlay } = this.$refs;
     whiteOverlay.style.width = "100%";
 
-    TweenMax.to(whiteOverlay, 1.1, {'opacity' : "0",
+    TweenMax.to(whiteOverlay, 1.1, {
+      opacity: "0",
       onComplete: setTimeout(() => {
         this.switchImage();
         whiteOverlay.style.display = "none";
-      }, 2000),
+      }, 2000)
     });
-
-    // setTimeout(() => {
-    //   this.switchImage();
-    //   // this.viaJs = require('./assets/img/image-slide-two.jpg') 
-    // }, imageSwitchInterval);
-    // //  this.startRotation();
   },
 
   methods: {
-    onSubmit(){
-      const formData = {
-        fullName : this.fullName,
-        email : this.email,
-        phoneNumber : this.phoneNumber
-      }
+    onSubmit() {
+      if (this.$v.$invalid) return;
 
-      axios.post('http://localhost:3005/api/users', formData)
-      .then(response => console.log(error))
-      .catch(error => console.log(error))
+      this.loading = true;
+
+      const data = {
+        full_name: this.full_name,
+        email_address: this.email_address,
+        phone_number: this.phone_number,
+        recieve_emails: this.recieve_emails
+      };
+
+      axios
+        .post("/api/users", data)
+        .then(({ data }) => {
+          this.loading = false;
+          const first_name = data.data.full_name.split(" ")[0];
+          const message = `Thanks for registering, ${upperFirst(
+            first_name
+          )}. We'll get back to you shortly.`;
+          this.scheduleToast(message, "success");
+        })
+        .catch(({ response }) => {
+          this.loading = false;
+          const { message } = response.data;
+          this.scheduleToast(message, "error");
+        });
     },
-    displayForm(){
+
+    scheduleToast(message, context) {
+      this.toast = { message, context, show: true };
+      setTimeout(() => {
+        this.toast = { message: "", context: "", show: false };
+      }, 3000);
+    },
+
+    displayForm() {
       this.showForm = true;
     },
-    switchImage(){
+
+    switchImage() {
       this.currentImg = images[imageIndex];
       imageIndex++;
-      
-      if(imageIndex == images.length){
+
+      if (imageIndex == images.length) {
         imageIndex = 0;
-      } 
-      // bigImage.src = require('./assets/img/image-slide-two.jpg') ;
-      //  document.getElementById("bigImage").style.display = "none";
+      }
 
       setTimeout(() => {
         this.switchImage();
       }, imageSwitchInterval);
-    },
-
-    // startRotation: function() {
-    //   this.timer = setInterval(this.next, 3000);
-    // },
-
-    // stopRotation: function() {
-    //   clearTimeout(this.timer);
-    //   this.timer = null;
-    // },
-
-    // next: function() {
-    //   this.currentNumber += 1
-    // },
-
-    // prev: function() {
-    //   this.currentNumber -= 1
-    // }
- 
+    }
   },
 
-  computed: {
-    // currentImage: function() {
-    //   return this.images[Math.abs(this.currentNumber) % this.images.length];
-    // }
+  components: {
+    LoopingRhombusesSpinner
   }
-  
-}
+};
 </script>
 
 <style lang="scss" scoped>
-
 @import "./assets/styles/sass/landing";
-
 </style>
