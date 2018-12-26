489121d238af410056b800df6fa4720fabe5619c
darthchudi <chukwudioranu@ymail.com>
2018-12-26 14:30:08 +0100

    feat: log request response and error

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,6 @@
 const httpStatus = require('http-status-codes');
 const User = require('../models/user');
+const Logger = require('../services/logger');
 
 exports.create = async (req, res) => {
   try {
@@ -9,9 +10,8 @@ exports.create = async (req, res) => {
       data: user,
       status: 'success',
     });
+    Logger.info({ res, user, meta_msg: 'successful registration' });
   } catch (err) {
-    //log here
-
     if (err && err.code === 11000)
       return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
         message: 'Oops! That email address has already been registered.',
@@ -22,5 +22,6 @@ exports.create = async (req, res) => {
       message: err.message,
       status: 'error',
     });
+    Logger.info({ res, err, meta_msg: 'user registration error' });
   }
 };
