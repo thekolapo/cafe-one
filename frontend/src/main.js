8a75a1da9979d8eee33809988776876f4e68f521
darthchudi <chukwudioranu@ymail.com>
2018-12-26 14:06:00 +0100

    feat: register vuelidate, toast globally

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,17 +1,22 @@
-import Vue from 'vue'
-import App from './App.vue'
-import VueRouter from 'vue-router'
-import { routes } from './routes'
+import Vue from 'vue';
+import Vuelidate from 'vuelidate';
+import App from './App.vue';
+import VueRouter from 'vue-router';
+import { routes } from './routes';
+import Toast from './Toast.vue';
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
+Vue.use(Vuelidate);
+
+Vue.component('toast', Toast);
 
 const router = new VueRouter({
   mode: 'history',
-  routes
-})
+  routes,
+});
 
 new Vue({
   el: '#app',
   router,
-  render: h => h(App)
-})
+  render: h => h(App),
+});
